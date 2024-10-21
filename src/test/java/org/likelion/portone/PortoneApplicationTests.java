b820bd0854dce491e0297a05e24ab13e27e7ef04
Kang Duoh <kduoh99@gmail.com>
2024-10-21 18:53:06 +0900

    🎉 Chore: 프로젝트 초기 세팅

---
+++ src/test/java/org/likelion/portone/PortoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.portone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
